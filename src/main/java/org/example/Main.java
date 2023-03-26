38653f7762d6660b1498e11e53d5996174967221
Piotr Kamola <45990692+PiotrKamola@users.noreply.github.com>
2023-03-26 19:10:22 +0200

    JJDZR11BB-1 - Sending request for lost or found objects.
    Sending request for lost or found objects

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,21 @@
 package org.example;
 
+import org.example.request.RequestController;
+import org.example.request.RequestMenu;
+import org.example.request.Request;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        RequestController requestController = new RequestController();
+        RequestMenu requestMenu = new RequestMenu(requestController);
+        requestMenu.sendRequestData();
+        requestMenu.sendRequestData();
+        //requestMenu.sendRequestData();
+
+
+        for (Request request : requestController.getAllRequests()) {
+            requestMenu.printRequest(request);
+        }
     }
 }
