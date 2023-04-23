1e938137bb705b1c974f83355f6c6b3e2c18ec85
PKamola <p.kamola98@wp.pl>
2023-04-23 20:16:04 +0200

    web

---
+++ web/src/test/java/pl/isa/BackendBoys/ZgubaApp/ZgubaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.isa.BackendBoys.ZgubaApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZgubaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
