b30a25436824241432c6412656b5c60ab363bfbf
PKamola <p.kamola98@wp.pl>
2023-05-21 20:52:05 +0200

    JJDZR11BB-19 & JJDZR11BB-21 & JJDZR11BB-23

--- web/src/main/java/pl/isa/backendBoys/zgubaAppWeb/rest/searchHelp.java
+++ web/src/main/java/pl/isa/backendBoys/zgubaAppWeb/rest/searchHelp.java
@@ -1,7 +1,43 @@
 package pl.isa.backendBoys.zgubaAppWeb.rest;
 
 public class searchHelp {
-    public String searchWord = new String();
+    private String searchWord = new String();
+    private String searchCity = new String();
+    private String searchDescription = new String();
+    private String searchLostOrFound = new String();
+    private String searchName = new String();
+
+    public String getSearchCity() {
+        return searchCity;
+    }
+
+    public void setSearchCity(String searchCity) {
+        this.searchCity = searchCity;
+    }
+
+    public String getSearchDescription() {
+        return searchDescription;
+    }
+
+    public void setSearchDescription(String searchDescription) {
+        this.searchDescription = searchDescription;
+    }
+
+    public String getSearchLostOrFound() {
+        return searchLostOrFound;
+    }
+
+    public void setSearchLostOrFound(String searchLostOrFound) {
+        this.searchLostOrFound = searchLostOrFound;
+    }
+
+    public String getSearchName() {
+        return searchName;
+    }
+
+    public void setSearchName(String searchName) {
+        this.searchName = searchName;
+    }
 
     public String getSearchWord() {
         return searchWord;
