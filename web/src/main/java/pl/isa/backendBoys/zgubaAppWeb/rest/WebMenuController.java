113a9b49c6d4c0041686392e97a920a7ad6ea6c5
PKamola <p.kamola98@wp.pl>
2023-05-28 18:54:51 +0200

    fixes in adding request and login

--- web/src/main/java/pl/isa/backendBoys/zgubaAppWeb/rest/WebMenuController.java
+++ web/src/main/java/pl/isa/backendBoys/zgubaAppWeb/rest/WebMenuController.java
@@ -5,14 +5,18 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.isa.backendBoys.zgubaAppWeb.request.RequestMenu;
 
 @Controller
 @RequestMapping("")
 public class WebMenuController {
+    RequestMenu requestMenu = new RequestMenu();
 
     @GetMapping("")
     public String menu(Model model) {
         model.addAttribute("searchWord", new SearchHelp());
+        model.addAttribute(requestMenu);
+//        requestToAdd.setRequesterLogin(requestMenu.loggedUserLogin);
         System.out.println();
         return "menu";
     }
