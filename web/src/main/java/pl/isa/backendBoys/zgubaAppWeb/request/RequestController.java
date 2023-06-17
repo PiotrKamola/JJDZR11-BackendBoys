fb5764f4a004e810e9e9c9d121610a3f216afb57
? <tplonski@outlook.com>
2023-06-17 20:19:04 +0200

    implement new templates to WebRequestController

--- web/src/main/java/pl/isa/backendBoys/zgubaAppWeb/request/RequestController.java
+++ web/src/main/java/pl/isa/backendBoys/zgubaAppWeb/request/RequestController.java
@@ -1,7 +1,11 @@
 package pl.isa.backendBoys.zgubaAppWeb.request;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+
 import java.util.List;
 
+@Component
 public class RequestController {
 
     private final RequestDatabase requestDatabase = new RequestDatabase();
--- web/src/main/java/pl/isa/backendBoys/zgubaAppWeb/rest/WebRequestController.java
+++ web/src/main/java/pl/isa/backendBoys/zgubaAppWeb/rest/WebRequestController.java
@@ -1,6 +1,5 @@
 package pl.isa.backendBoys.zgubaAppWeb.rest;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,31 +11,31 @@ import pl.isa.backendBoys.zgubaAppWeb.request.RequestController;
 import pl.isa.backendBoys.zgubaAppWeb.search.SearchController;
 import pl.isa.backendBoys.zgubaAppWeb.user.UserController;
 
-import java.util.List;
-
 @Controller
 @RequestMapping("request")
 public class WebRequestController {
 
-    final RequestController requestController = new RequestController();
-    final SearchController searchController = new SearchController();
+    private final UserController userController;
+    private final RequestController requestController;
+    private final SearchController searchController;
 
-    final
-    UserController userController;
-
-    public WebRequestController(UserController userController) {
+    public WebRequestController(RequestController requestController, SearchController searchController, UserController userController) {
+        this.requestController = requestController;
+        this.searchController = searchController;
         this.userController = userController;
     }
 
     @GetMapping("/all")
     public String allRequests(Model model) {
         model.addAttribute("allRequests", requestController.getAllRequests());
+        model.addAttribute("loggedUser", userController.getLoggedUserEmail());
         model.addAttribute("content", "allRequests");
         return "main";
     }
 
     @GetMapping("/submitted")
     public String submittedRequest(Model model) {
+        model.addAttribute("loggedUser", userController.getLoggedUserEmail());
         model.addAttribute("content", "submittedRequest");
         return "main";
     }
@@ -44,36 +43,37 @@ public class WebRequestController {
     @PostMapping("/submitted")
     public String addNewRequest(Model model, @ModelAttribute Request requestToAdd) {
         model.addAttribute(requestToAdd);
+        model.addAttribute("loggedUser", userController.getLoggedUserEmail());
         requestToAdd.setRequesterLogin(userController.getLoggedUserEmail());
         requestController.addRequest(requestToAdd);
         requestToAdd.nicePrint();
-        model.addAttribute("content", "submittedRequest");
+        model.addAttribute("content", "SubmittedRequest");
         return "main";
     }
 
     @GetMapping("/add")
     public String addRequest(Model model) {
         model.addAttribute("requestToAdd", new Request());
+        model.addAttribute("loggedUser", userController.getLoggedUserEmail());
         model.addAttribute("enum", Request.LostOrFound.class.getName());
         model.addAttribute("content", "addRequest");
         return "main";
     }
 
 
-
-    @PostMapping("/search")
-    public String search(Model model, @ModelAttribute SearchHelp searchWord) {
-        model.addAttribute(searchWord);
-
-        List<Request> searchList = searchController.searchByWord(requestController.getAllRequests(), searchWord.getSearchWord());
-        for (Request req : searchList) {
-            req.nicePrint();
-            System.out.println("---");
-        }
-
-        model.addAttribute("allRequests", searchList);
-        model.addAttribute("content", "allRequests");
-        return "main";
-
-    }
+//    @PostMapping("/search")
+//    public String search(Model model, @ModelAttribute SearchHelp searchWord) {
+//        model.addAttribute(searchWord);
+//
+//        List<Request> searchList = searchController.searchByWord(requestController.getAllRequests(), searchWord.getSearchWord());
+//        for (Request req : searchList) {
+//            req.nicePrint();
+//            System.out.println("---");
+//        }
+//
+//        model.addAttribute("allRequests", searchList);
+//        model.addAttribute("content", "allRequests");
+//        return "main";
+//
+//    }
 }
